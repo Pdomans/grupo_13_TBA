74156a272f19829e7faa2617863bea433d96f508
cristianGiraldo12 <alduvier1225@gmail.com>
2023-02-28 20:05:51 -0500

    actualizacion de loggin y del app

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ app.use(express.static('public'));
 
 
 app.get ("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "src/views/home.html"))
-})
+    res.sendFile(path.join(__dirname, "src/views/home.html"));
+});
+
+app.get("/loggin", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/views/loggin.html"));
+});
 
 app.listen(3500,() => {
-    console.log ("Servidor andando");
+    console.log ("Servidor andando en el puerto 3500");
 });
 
