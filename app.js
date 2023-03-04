dffe2c883aca4e033cb22375193c75adfedf8454
cristianGiraldo12 <alduvier1225@gmail.com>
2023-03-04 00:17:32 -0500

    headder v1, app actualizado con las diferentes rutas

--- app.js
+++ app.js
@@ -14,7 +14,20 @@ app.get("/loggin", (req, res) => {
     res.sendFile(path.join(__dirname, "src/views/loggin.html"));
 });
 
+app.get("/productos", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/views/productos.html"));
+});
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/views/carrito.html"));
+});
+
+app.get("/quienes-somos", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/views/quiensomos.html"));
+});
+
+
 app.listen(3500,() => {
-    console.log ("Servidor andando en el puerto 3500");
+    console.log ("Servidor corriendo en el puerto 3500");
 });
 
