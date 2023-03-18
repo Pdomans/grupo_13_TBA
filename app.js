9248c1bc64639f89cf70668d6346d8c0e451ae8a
ericpino87 <ericpinonajarro87@gmail.com>
2023-03-18 18:07:38 -0300

    cambios en las rutas dentro de carpeta controllers y routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require ("express");
 const path = require("path");
-
 const app = express();
+
+const { inicio, carrito, productos, quiensomos, detalleProducto } = require("./src/controllers/mainController");
+
 
 app.use(express.static('public'));
 
@@ -34,4 +36,17 @@ app.get("/quienes-somos", (req, res) => {
 app.listen(3500,() => {
     console.log ("Servidor corriendo en el puerto http://localhost:3500");
 });
+
+
+
+// Importamos los distintos enrutadores
+const mainRouter = require("./routes/mainRouter.js")
+const productsRouter = require("./routes/productsRouter.js")
+const userRouter = require("./routes/userRouter.js")
+
+// Usando los enrutadores importados
+app.use("/", mainRouter);
+app.use("/productos", productsRouter);
+app.use("/usuario", userRouter);
+
 
