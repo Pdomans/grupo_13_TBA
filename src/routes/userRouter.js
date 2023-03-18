7dc679a48a1155f75efb793ccc735446f5db8de0
ericpino87 <ericpinonajarro87@gmail.com>
2023-03-18 17:08:56 -0300

    cambios en las rutas dentro de carpeta controllers y routes

---
+++ src/routes/userRouter.js
@@ -0,0 +1,16 @@
+// Requerimos express y guardamos la ejecución del método Router, que usaremos en el archivo.
+const express = require("express");
+const router = express.Router();
+
+// Importamos el controlador de usuarios
+const usersController = require("../controllers/usersController.js");
+
+// En vez de app.get, utilizamos router.get. Esto va "guardando" en router las distintas rutas, que luego exportamos
+
+// Procesa el pedido get con ruta /usuarios/registrarse
+router.get("/registro", usersController.registro)
+// Procesa el pedido get con ruta /usuarios/conectarse
+router.get("/login", usersController.login)
+
+// Exportamos la variable router ya con todas las rutas "guardadas", que se usará en app.js
+module.exports = router;
