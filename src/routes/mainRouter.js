be510f3182f6b95ebc227367825556d5a6145fc5
ericpino87 <ericpinonajarro87@gmail.com>
2023-03-18 16:29:32 -0300

    mainRouter created

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -9,7 +9,12 @@ const mainController = require("../controllers/mainController.js")
 // En vez de app.get, utilizamos router.get. Esto va "guardando" en router las distintas rutas, que luego exportamos
 
 // Procesa el pedido get con ruta /
-router.get("/", mainController.index);
+router.get("/", mainController.inicio);
+router.get("/carrito", mainController.carrito);
+router.get("/login", mainController.login);
+router.get("/productos", mainController.productos);
+router.get("/quiensomos", mainController.quiensomos);
+router.get("/registro", mainController.registro);
 
 // Exportamos la variable router ya con todas las rutas "guardadas", que se usará en app.js
 module.exports = router;
