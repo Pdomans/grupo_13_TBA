d99a63fc759ca8c241059837e7432e0972e5ef36
ericpino87 <ericpinonajarro87@gmail.com>
2023-03-18 16:15:32 -0300

    mainController created

--- src/controllers/mainControler.js
+++ src/controllers/mainControler.js
@@ -9,6 +9,36 @@ const mainController = {
         // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/inicio.html"))
     }
+    // Manejo del pedido get con ruta
+    carrito: (req, res) => {
+        // comunicarse con el modelo, conseguir información
+        res.sendFile(path.resolve(__dirname, "../views/carrito.html"))
+    }
+        // Manejo del pedido get con ruta
+    login: (req, res) => {
+        // comunicarse con el modelo, conseguir información
+        res.sendFile(path.resolve(__dirname, "../views/login.html"))
+    }
+    // Manejo del pedido get con ruta
+    productos: (req, res) => {
+        // comunicarse con el modelo, conseguir información
+        res.sendFile(path.resolve(__dirname, "../views/productos.html"))
+    }
+    // Manejo del pedido get con ruta
+    quiensomos: (req, res) => {
+        // comunicarse con el modelo, conseguir información
+        res.sendFile(path.resolve(__dirname, "../views/quiensomos.html"))
+    }
+        // Manejo del pedido get con ruta
+    registro: (req, res) => {
+        // comunicarse con el modelo, conseguir información
+        res.sendFile(path.resolve(__dirname, "../views/registro.html"))
+    }
+        // Manejo del pedido get con ruta
+    detalleProducto: (req, res) => {
+        // comunicarse con el modelo, conseguir información
+        res.sendFile(path.resolve(__dirname, "../views/detalleProducto.html"))
+    }
 }
 
 // Exportamos el objeto literal con los distintos métodos, que se usará en el enrutador por defecto
