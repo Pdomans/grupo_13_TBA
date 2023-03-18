d298332e5d57994750584eede1a2167333f6a89b
ericpino87 <ericpinonajarro87@gmail.com>
2023-03-18 16:16:05 -0300

    mainController created

--- src/controllers/mainControler.js
+++ src/controllers/mainControler.js
@@ -9,34 +9,22 @@ const mainController = {
         // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/inicio.html"))
     }
-    // Manejo del pedido get con ruta
     carrito: (req, res) => {
-        // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/carrito.html"))
     }
-        // Manejo del pedido get con ruta
     login: (req, res) => {
-        // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/login.html"))
     }
-    // Manejo del pedido get con ruta
     productos: (req, res) => {
-        // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/productos.html"))
     }
-    // Manejo del pedido get con ruta
     quiensomos: (req, res) => {
-        // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/quiensomos.html"))
     }
-        // Manejo del pedido get con ruta
     registro: (req, res) => {
-        // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/registro.html"))
     }
-        // Manejo del pedido get con ruta
     detalleProducto: (req, res) => {
-        // comunicarse con el modelo, conseguir información
         res.sendFile(path.resolve(__dirname, "../views/detalleProducto.html"))
     }
 }
