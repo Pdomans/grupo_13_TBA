31b0da105a410519d337631fd7f07aa09f9b6a14
pdomans <pablodomanski90@gmail.com>
2023-05-27 11:50:58 -0300

    correccion middle

--- src/middlewares/global/middle.js
+++ src/middlewares/global/middle.js
@@ -1,10 +1,8 @@
 function testmiddleware(req,res,next){
 
-<<<<<<< HEAD
-    console.log("middle prueba 27-may-2023 "); 
-=======
-    console.log("middle prueba abc 1"); 
->>>>>>> 896336c248996351e98e9db4584d407ff4fbfad2
+
+console.log("middle prueba 27-may-2023 "); 
+
     next();
 }
 
