21c882ada7c174f264f6043e45db6d329a2fd8bb
ericpino87 <ericpinonajarro87@gmail.com>
2023-06-13 08:33:18 -0300

    creacion de columnas en archivo models Factura.js

--- database/models/Factura.js
+++ database/models/Factura.js
@@ -1,1 +1,59 @@
-console.log ('aca estoy eric ');
+module.exports =function (sequelize,datatypes){
+  
+    let alias="Factura";
+  
+    let cols = {
+  
+      id_factura: {
+        type:datatypes.INTEGER,
+        primaryKey:true,
+        autoIncrement: true,
+        },
+
+        id_user: {
+          type:datatypes.INTEGER,
+        },
+
+        id_product: {
+          type:datatypes.INTEGER,
+        },
+  
+        fecha:{
+        type :datatypes.DATE,
+        },
+  
+        cant:{
+        type :datatypes.INTEGER,   
+        },
+  
+        precio:{
+        type :datatypes.INTEGER,
+        }
+  
+       
+      }
+  
+        let config = {
+          tableName: "factura",
+          timestamps: false
+        
+    }
+  
+      let Factura = sequelize.define(alias,cols,config);
+  
+      Factura.associate = function (models){
+
+        Factura.belongsToMany(models.Producto,{
+          as: "categoria_usuario",
+          foreignKey: "id_product"  
+        })
+
+          Factura.belongsTo(models.User,{
+            as: "user",
+            foreignKey: "id_user"
+      })
+
+    }
+      return Factura;
+  
+}
