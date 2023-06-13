c938f692724802b1b548e8da2db7f37296de1d3f
pdomans <pablodomanski90@gmail.com>
2023-06-12 21:51:21 -0300

    modelo productos

--- database/models/productos.js
+++ database/models/productos.js
@@ -1,4 +1,47 @@
 module.exports =function (sequelize,datatypes){
- let producto = sequelize.define
+    let alias="Producto";
+
+    let cols = {
+        
+   id : {
+    type:datatypes.INTEGER,
+    primaryKey:true,
+
+   },
+
+   name_product :{
+    
+    type :datatypes.string
+   
+    },
+
+    name_product :{
+        type:datatypes.string
+    },
+    image :{
+        type:datatypes.integer
+    },
+    description_product:{
+        type:datatypes.string
+    }, 
+    price :{
+        type:datatypes.integer
+    },
+    disctount:{
+        type:datatypes.integer
+    },              
+    stock:{
+        type:datatypes.integer
+    },
+    id_type_product :{
+        type:datatypes.integer
+    }
+
+
+    }
+
+    let producto = sequelize.define(alias,cols,config);
+
+ return 
 
 }
