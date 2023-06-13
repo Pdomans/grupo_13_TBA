1fde7e3f85ab07c27fbc1e92d7577a929e6e48ce
ericpino87 <ericpinonajarro87@gmail.com>
2023-06-12 22:25:27 -0300

    creacion de columnas en archivo models User.js

---
+++ database/models/User.js
@@ -0,0 +1,43 @@
+module.exports =function (sequelize,datatypes){
+    let alias="User";
+
+    let cols = {
+        
+
+
+    id_user: {
+      type:datatypes.INTEGER,
+      primaryKey:true,
+    },
+
+    firstname:{
+    type :datatypes.string,
+    },
+
+    lastname:{
+    type :datatypes.string,
+   
+    },
+    mail:{
+    type :datatypes.string,
+   
+    },
+    password:{
+    type :datatypes.string,
+   
+    },
+    image :{
+    type :datatypes.string,
+   
+    },
+    
+    id_category:{
+    type :datatypes.string,
+    }
+
+
+    }
+  let producto = sequelize.define(alias,cols,config);
+
+  return 
+    }
